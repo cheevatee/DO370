e6489ca0fb279ca4591746e01d6f10d26575e6aa
Student User <student@workstation.lab.example.com>
2022-03-21 11:35:32 -0400

    add uninstall-odf.sh

---
+++ chapter1/uninstall-odf.sh
@@ -0,0 +1,13 @@
+oc get volumesnapshot --all-namespaces
+oc get obc --all-namespaces
+read aaa
+oc project openshift-storage
+oc annotate storagecluster ocs-storagecluster uninstall.ocs.openshift.io/cleanup-policy="delete" --overwrite
+oc annotate storagecluster ocs-storagecluster uninstall.ocs.openshift.io/mode="graceful" --overwrite
+oc delete storagecluster --all --wait=true
+oc project default
+oc delete project openshift-storage --wait=true --timeout=5m
+oc label nodes --all cluster.ocs.openshift.io/openshift-storage-
+oc label nodes --all topology.rook.io/rack-
+oc delete sc openshift-storage.noobaa.io --wait=true --timeout=5m
+oc delete crd
