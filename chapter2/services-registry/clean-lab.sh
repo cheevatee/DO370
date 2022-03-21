744503c2b4021338205b7d392fbafcdab5011499
Student User <student@workstation.lab.example.com>
2022-03-21 14:56:39 -0400

    Update clean-lab.sh

--- chapter2/services-registry/clean-lab.sh
+++ chapter2/services-registry/clean-lab.sh
@@ -1,5 +1,8 @@
 oc apply -f configs-imageregistry-cluster.yaml -n openshift-image-registry
 oc delete obc noobaa-registry -n openshift-image-registry
-
+oc delete secret image-registry-private-configuration-user -n openshift-image-registry
+oc delete project services-registry
+oc delete jobs awscli -n openshift-image-registry
+oc label nodes env- -l env=qa
 
 oc get po -n openshift-image-registry
