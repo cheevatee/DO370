bd3cfec36d4b16a8b1f1cbd4ff57e4c3abaf86c5
Student User <student@workstation.lab.example.com>
2022-03-22 10:20:11 -0400

    Add workloads-block

---
+++ chapter3/workloads-block/clean-lab.sh
@@ -0,0 +1,3 @@
+
+
+oc delete project image-app
---
+++ chapter3/workloads-block/start-lab.sh
@@ -0,0 +1,2 @@
+cp deployment.yaml /home/student/DO370/labs/workloads-block/deployment.yaml
+cp nginx.yaml /home/student/DO370/labs/workloads-block/nginx.yaml
