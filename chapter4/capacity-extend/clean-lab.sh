5ecdcb37f38164a74722737b5e2171147516a4ec
Student User <student@workstation.lab.example.com>
2022-03-22 13:05:02 -0400

    Add capacity-extend

---
+++ chapter4/capacity-extend/clean-lab.sh
@@ -0,0 +1,5 @@
+
+
+oc delete project capacity-extend-ge
+oc delete template postgresql-persistent-sc -n openshift
+rm -rf ~/DO370/labs/capacity-extend
--- chapter4/capacity-quotas/clean-lab.sh
+++ chapter4/capacity-quotas/clean-lab.sh
@@ -1,3 +1,4 @@
 
 
 oc delete project capacity-quotas
+rm -rf ~/DO370/labs/capacity-quotas
