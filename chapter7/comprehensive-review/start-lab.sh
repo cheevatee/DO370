551a17d7df280abb097114a6bdca55474f75bd37
Student User <student@workstation.lab.example.com>
2022-03-24 13:17:51 -0400

    Add comprehensive-review

---
+++ chapter7/comprehensive-review/start-lab.sh
@@ -0,0 +1,2 @@
+cp object-storage-app.yaml /home/student/DO370/labs/comprehensive-review/object-storage-app.yaml
+cp image-tool-app.yaml /home/student/DO370/labs/comprehensive-review/image-tool-app.yaml
